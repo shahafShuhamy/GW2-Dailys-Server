19ad636170f8ea50d7bfa42590e54a4276deb3fe
ShahafShuhamy <Shahaf761@gmail.com>
2018-08-06 10:04:50 +0300

    add tasked get Dailies method at 10AM everyday

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const mongoose = require('./db/mongo').mongoose;
 const AnetClient = require('./AnetClient');
 const {DailyAchivement} = require('./db/models/Daily');
 const utils = require('./utils');
+var scheduler = require('node-schedule');
+
+var j = scheduler.scheduleJob('0 0 10 * * *', () => {
+    getDailiesAndPutInDB();
+});
 
 //Saving Daily Achivements Should be at 4Am 
 //Data inserting to mongo is with UTC time zone which is 3 hours before Israel locale timezone.
-//var todayDate = new Date().toLocaleTimeString('he-IL'); -> to get localtime hour.
+var getDailiesAndPutInDB = () =>{
 AnetClient.getDailies((dailiesBefore) =>{
     var listToMongo = [];
     if(dailiesBefore){
@@ -25,7 +30,7 @@ AnetClient.getDailies((dailiesBefore) =>{
             }else{
                 console.log('saved Data base list');
             }
-            process.exit(1); //last callback method in program
         });    
         }
     });
+}
